84bcc7491752a123da9c664611d76254a71786f4
stefano <stefanofg@gmail.com>
2014-08-03 19:00:33 -0700

    Refctored Template classes and updated html fragments

--- controller/template.php
+++ controller/template.php
@@ -21,7 +21,7 @@ abstract class AbstractTemplate
 
 
 
-    protected function renderContent($html_content)
+    protected function renderHtmlFragment($html_content)
     {
 
 
@@ -32,21 +32,119 @@ abstract class AbstractTemplate
 
     abstract public function render();
 
-}
+};
 
-class LandingPage extends AbstractTemplate
+
+
+
+class StandardPageTemplate extends AbstractTemplate
 {
+    protected $header,$content,$footer,$menubar;
+    protected $bodyClass;
 
+
+    public function __construct($f3,$view)
+    {
+        parent::__construct($f3,$view);
+
+
+    }
     public function render()
     {
-        $this->renderContent("ui/head.html");
+        // render header if available
+        if($this->header)
+            $this->renderHtmlFragment($this->header);
+        else
+            echo "<head></head>";
+
+        $bodyTag="<body>\n";
+        // begin body
+        if($this->bodyClass)
+            $bodyTag="<body "." class='".$this->bodyClass."'>\n";
+        echo $bodyTag;
         echo "<div class=\"content\">\n";
-        $this->renderContent("ui/menubar.html");
-        $this->renderContent("ui/landing-content.html");
-        echo "</div>\n";
-        $this->renderContent("ui/footer.html");
-        $this->renderContent("ui/bottom-scripts.html");
+
+        // render menubar if available
+        if($this->menubar)
+            $this->renderHtmlFragment($this->menubar);
+        else echo "<div></div>";
+
+        // render content if available
+        if($this->content)
+            $this->renderHtmlFragment($this->content);
+        else echo "<div></div>";
+
+        echo "</div>\n"; // end of content container
+
+        // render footer if available
+        if($this->footer)
+            $this->renderHtmlFragment("ui/footer.html");
+
+        // scripts are needed anyway for stylio to work
+        $this->renderHtmlFragment("ui/bottom-scripts.html");
+
+        // init all the scripts
         echo "<script>initStylio();</script>";
+
+        // end of the page
         echo "</body></html>";
     }
+
+    public function setFooter($html)
+    {
+       $this->footer=$html;
+    }
+
+    public function setHeader($html)
+    {
+        $this->header=$html;
+    }
+
+    public function setContent($html)
+    {
+        $this->content=$html;
+    }
+
+    public function setMenubar($html)
+    {
+        $this->menubar=$html;
+    }
+
+    public function setBodyClass($class)
+    {
+        $this->bodyClass=$class;
+    }
 }
+
+class LandingPage extends StandardPageTemplate
+{
+
+    public function __construct($f3,$view)
+    {
+        parent::__construct($f3,$view);
+        $this->setHeader("ui/head.html");
+        $this->setMenubar("ui/menubar.html");
+        $this->setContent("ui/landing-content.html");
+        $this->setFooter("ui/footer.html");
+
+    }
+
+
+};
+
+class RegisterPage extends StandardPageTemplate
+{
+
+    public function __construct($f3,$view)
+    {
+        parent::__construct($f3,$view);
+        $this->setHeader("ui/head.html");
+        $this->setMenubar("ui/menubar.html");
+        $this->setContent("ui/register-content.html");
+        $this->setBodyClass("register-page");
+
+
+    }
+
+
+};
--- index.php
+++ index.php
@@ -19,7 +19,26 @@ $f3->route('GET /',
 $f3->route('GET /register',
     function($f3) {
 
+        $page = new RegisterPage($f3,View::instance());
+        $page->render();
+    }
+);
 
+$f3->route('GET /register2',
+    function($f3) {
+
+        $page = new RegisterPage($f3,View::instance());
+        $page->setContent("ui/register2-content.html");
+        $page->render();
+    }
+);
+
+$f3->route('GET /register3',
+    function($f3) {
+
+        $page = new RegisterPage($f3,View::instance());
+        $page->setContent("ui/register3-content.html");
+        $page->render();
     }
 );
 
