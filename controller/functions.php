242064fa20b0b5681a6b441152b1d8db7f5ec652
stefano <stefanofg@gmail.com>
2014-08-04 19:16:05 -0700

    more work to make the website completely dynamic

---
+++ controller/functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stefano
+ * Date: 8/4/14
+ * Time: 6:13 PM
+ */
+
+function renderWaitingListForm($view)
+{
+
+    return $view->render('ui/add-wait-list-form.html');
+}
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once('controller/template.php');
 $f3= require('lib/base.php');
 
+
 $f3->set('DEBUG',1);
 if ((float)PCRE_VERSION<7.9)
 	trigger_error('PCRE version is out of date');
@@ -18,26 +19,89 @@ $f3->route('GET /',
 
 $f3->route('GET /register',
     function($f3) {
-
-        $page = new RegisterPage($f3,View::instance());
-        $page->render();
-    }
-);
-
-$f3->route('GET /register2',
-    function($f3) {
-
+        $f3->set('btnLabel','Let me know about GryfenCoin news!');
         $page = new RegisterPage($f3,View::instance());
         $page->setContent("ui/register2-content.html");
         $page->render();
     }
 );
 
-$f3->route('GET /register3',
+$f3->route('GET /gryfencoin-proof-of-work-mining',
     function($f3) {
 
-        $page = new RegisterPage($f3,View::instance());
-        $page->setContent("ui/register3-content.html");
+        $f3->set('btnLabel','Add me to the wait list');
+        $page = new LandingPage($f3,View::instance());
+        $page->setContent("ui/gryfencoin-proof-of-work-mining.html");
+        $page->render();
+    }
+);
+
+$f3->route('GET /gryfencoin-proof-of-stake-minting',
+    function($f3) {
+
+        $f3->set('btnLabel','Add me to the wait list');
+        $page = new LandingPage($f3,View::instance());
+        $page->setContent("ui/gryfencoin-proof-of-stake-minting.html");
+        $page->render();
+    }
+);
+
+$f3->route('GET /gryfencoin-super-faucet-minting',
+    function($f3) {
+
+        $f3->set('btnLabel','Add me to the wait list');
+        $page = new LandingPage($f3,View::instance());
+        $page->setContent("ui/gryfencoin-super-faucet-minting.html");
+        $page->render();
+    }
+);
+
+$f3->route('GET /gryfencoin-stake-share-futures',
+    function($f3) {
+
+        $f3->set('btnLabel','Add me to the wait list');
+        $page = new LandingPage($f3,View::instance());
+        $page->setContent("ui/gryfencoin-stake-share-futures.html");
+        $page->render();
+    }
+);
+
+$f3->route('GET /gryfen-co-cryptocurrency-options-market-and-exchange',
+    function($f3) {
+
+        $f3->set('btnLabel','Add me to the wait list');
+        $page = new LandingPage($f3,View::instance());
+        $page->setContent("ui/gryfen-co-cryptocurrency-options-market-and-exchange.html");
+        $page->render();
+    }
+);
+
+$f3->route('GET /gryfen-options-market-cloud-multipool',
+    function($f3) {
+
+        $f3->set('btnLabel','Add me to the wait list');
+        $page = new LandingPage($f3,View::instance());
+        $page->setContent("ui/gryfen-options-market-cloud-multipool.html");
+        $page->render();
+    }
+);
+
+$f3->route('GET /gryfen-business-and-projects',
+    function($f3) {
+
+        $f3->set('btnLabel','Add me to the wait list');
+        $page = new LandingPage($f3,View::instance());
+        $page->setContent("ui/gryfen-business-and-projects.html");
+        $page->render();
+    }
+);
+
+$f3->route('GET /gryfencoin-awesome-users',
+    function($f3) {
+
+        $f3->set('btnLabel','Add me to the wait list');
+        $page = new LandingPage($f3,View::instance());
+        $page->setContent("ui/gryfencoin-awesome-users.html");
         $page->render();
     }
 );
